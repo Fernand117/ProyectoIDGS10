5d86c65d6ecf91a4cc95c1d5d9840fc4333f0251
Adriana Rodriguez <20213l001130@utcv.edu.mx>
2025-09-27 12:07:48 -0600

    mensaje de adrfiana

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = requite('express');
+const express = require('express');
 const app = express();
 const port = 3000;
 
+// Nueva ruta raíz
+app.get('/', (req, res) => {
+  res.send('¡Hola! Esta es mi primera API con Express 🚀');
+});
+
 app.listen(port, () => {
-  console.log(`API TEST`);
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
