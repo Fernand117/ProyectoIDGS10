d49e7c5410f0c652e9b71e11ad157a4cfb37a213
Llaned02 <nanybb2233@gmail.com>
2025-09-27 13:12:54 -0600

    hola

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = requite('express');
+const express = require('express'); // corregido 'requite' a 'require'
 const app = express();
 const port = 3000;
 
+// Ruta raíz
+app.get('/', (req, res) => {
+  res.send('¡Hola desde la API!');
+});
+
+// Inicio del servidor
 app.listen(port, () => {
-  console.log(`API TEST`);
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
