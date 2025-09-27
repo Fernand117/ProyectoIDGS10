3d65c3bbd3eac72b93f1fa84ab21f06707eddf89
Jair <20213l001096@utcv.edu.mx>
2025-09-27 12:10:39 -0600

    modificar index

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`API TEST`);
 });
+function saludo(nombre) {
+  const fecha = new Date().toLocaleString();
+  return `Hola, ${nombre}. Hoy es ${fecha}.`;
+}
+
+console.log(saludo("jair"));
